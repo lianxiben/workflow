51ff558133326e0333969ef2a1b98b495dd36bb2
lianxiben <source@comic.com>
2025-07-10 09:51:29 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { initScrollReveal } from './js/startanimation.js';
 // 配置参数
 export const maxDays = 60;                 // 日志最大展示天数
 export const maxHour = 12;                 // 报表最大小时数
-export const urlspath = "/src/urls.cfg";  // 配置文件路径,不带后/
+export const urlspath = "./src/urls.cfg";  // 配置文件路径,不带后/
 export const logspath = "./logs";          // 日志文件路径,不带后/
 export const reloadReportsdata = false;     // 是否重新加载报告
 export const reloadReportstime = 2.5;        // 重载报告的检测间隔时间
