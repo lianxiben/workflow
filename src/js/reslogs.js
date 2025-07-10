8f6a047c1a947350f2b3497af6263c9d363098cf
lianxiben <source@comic.com>
2025-07-10 09:16:23 +0800

    Add files via upload

---
+++ src/js/reslogs.js
@@ -0,0 +1,33 @@
+import { logspath } from "../index.js";
+
+/**
+ * 异步函数：获取日志文件内容。
+ * 
+ * 该函数通过HTTP请求从指定的URL获取日志文件的内容。如果请求成功，它将返回日志文本；
+ * 如果请求失败，它将抛出一个错误。这个函数使用了fetch API来进行网络请求，并支持使用缓存。
+ * 
+ * @param {string} key - 日志文件名的关键字，用于构造URL。
+ * @param {Object} uesCache - 控制是否使用缓存的对象，默认为使用'default'缓存策略。该参数的具体作用取决于fetch函数的实现。
+ * @returns {Promise<string>} - 返回一个承诺，该承诺解析为日志文件的文本内容。
+ * @throws {Error} - 如果请求失败，将抛出一个包含错误信息的异常。
+ */
+export async function reslogs(key, useCache = { cache: 'default' }) {
+  const url = logspath + "/" + key + "_report.log";
+  const urlB = "./logs/" + key + "_report.log"; // 备选logspath
+
+  try {
+    const response = await fetch(url, useCache);
+    // 如果请求失败，使用备选logspath
+    if (!response.ok) {
+      console.warn('Fetch failed. Attempting to use the alternate logspath.');
+      const responseB = await fetch(urlB, useCache);
+      const responsetext = await responseB.text();
+      return responsetext;
+    }
+    // 请求成功，返回文本
+    const responsetext = await response.text();
+    return responsetext;
+  } catch (error) {
+    console.error(error);
+  }
+}
---
+++ src/js/utils.js
@@ -0,0 +1,102 @@
+import { templatize } from "./domManipulation.js";
+import { showTooltip, hideTooltip } from "./tooltip.js";
+import { maxDays } from "../index.js";
+/**
+ * 获取颜色
+ * @param {any} uptimeVal - 运行时间值
+ * @returns {string} - 颜色字符串
+ */
+export function getColor(uptimeVal) {
+  return uptimeVal == null
+    ? "nodata"
+    : uptimeVal == 1
+      ? "success"
+      : uptimeVal < 0.3
+        ? "failure"
+        : "partial";
+}
+
+/**
+ * 获取状态文本
+ * @param {string} color - 颜色
+ * @returns {string} - 状态文本
+ */
+export function getStatusText(color) {
+  return color == "nodata"
+    ? "No Data"
+    : color == "success"
+      ? "up"
+      : color == "failure"
+        ? "Down"
+        : color == "partial"
+          ? "Degraded"
+          : "Unknown";
+}
+
+/**
+ * 构建状态流
+ * @param {string} url - URL
+ * @param {Object} uptimeData - 运行时间数据
+ */
+export function constructStatusStream(key, url, uptimeData) {
+  let streamContainer = templatize("statusStreamContainerTemplate");
+  for (var ii = maxDays - 1; ii >= 0; ii--) {
+    let line = constructStatusLine(ii, uptimeData[ii]);
+    streamContainer.appendChild(line);
+  }
+  const lastSet = uptimeData[0];
+  const color = getColor(lastSet);
+  const statusText = getStatusText(color);
+
+  // 创建 img 元素
+  const img = document.createElement('img');
+  img.className = 'statusIcon';
+  img.alt = `status${color}`;
+  img.src = `./public/check/${color}.svg`;
+
+  const container = templatize("statusContainerTemplate", {
+    title: key,
+    url: url,
+    statusblock: statusText,
+    upTime: uptimeData.upTime,
+  });
+
+  // 将img元素插入到statusTitle之前。
+  const parent = container.querySelector('#statusTitle').parentNode;
+  parent.insertBefore(img, parent.querySelector('#statusTitle'));
+
+  container.appendChild(streamContainer);
+
+  // console.log(container)
+
+  return container;
+}
+
+/**
+ * 构建状态行
+ * @param {number} relDay - 相对天数
+ * @param {any} upTimeArray - 运行时间数组
+ * @returns {HTMLElement} - 状态行元素
+ */
+function constructStatusLine(relDay, upTimeArray) {
+  let date = new Date();
+  date.setDate(date.getDate() - relDay);
+  return constructStatusSquare(date, upTimeArray);
+}
+
+function constructStatusSquare(date, uptimeVal) {
+  const color = getColor(uptimeVal);
+  let square = templatize("statusSquareTemplate", {
+    color: color,
+  });
+  const show = () => {
+    showTooltip(square, date, color);
+  };
+  const hide = () => {
+    hideTooltip(square);
+  };
+  square.addEventListener("mouseover", show);
+  square.addEventListener("mousedown", show);
+  square.addEventListener("mouseout", hide);
+  return square;
+}
